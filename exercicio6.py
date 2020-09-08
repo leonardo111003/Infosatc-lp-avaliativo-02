b34fc7e21520df06b474fbd1b4c13586cb48ab58
leonardo111003 <leocatra123@hotmail.com>
2020-09-08 10:17:26 -0300

    feat: adiciona exercicio 6

---
+++ exercicio6.py
@@ -0,0 +1,8 @@
+filmes   = [ "velozes e furiosos" , "missão impossivel" ]
+jogos    = [ " Fifa20", " Formula 1 2020"   ]
+livros   = [  "Tite" , "pep guardiola confidencial" ]
+esporte  = [ "Futebol" , "volei" , ]
+listas  =  filmes  +  livros  +  jogos  +  esporte
+print ( listas )
+para  livro  em  livros :
+    print ( livro )   
